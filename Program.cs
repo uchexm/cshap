d87b553e848d757cffd3c9465c996180a1344a89
uchexm <109084069+uchexm@users.noreply.github.com>
2023-09-17 13:18:16 +0200

    arrays

--- Program.cs
+++ Program.cs
@@ -1,20 +1,10 @@
-﻿/*string[] fraudulentIds = new string[3];
-
-fraudulentIds[0] = "A123";
-
-fraudulentIds[1] = "B456";
-
-fraudulentIds[2] = "C789";*/
-
-string[] fraudulentIds = {"A123", "B456", "C789"};
-
-
-Console.WriteLine($"First: {fraudulentIds[0]}");
-Console.WriteLine($"Second: {fraudulentIds[1]}");
-Console.WriteLine($"Third: {fraudulentIds[2]}");
-
-fraudulentIds[0] = "F00456";
-
-Console.WriteLine($"Reassigned First: {fraudulentIds[0]}");
-
-Console.WriteLine($"There are {fraudulentIds.Length} fraudulent orders");
+﻿int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+int bin = 0;
+foreach(int items in inventory){
+  sum += items;
+  bin++;
+  Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+}
+// Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+Console.WriteLine($"We have {sum} items in inventory.");
