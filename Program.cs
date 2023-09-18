688c7a6105264b64b393ff46b559ff275e777821
uchexm <109084069+uchexm@users.noreply.github.com>
2023-09-18 13:27:14 +0200

    arrays

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿int[] inventory = { 200, 450, 700, 175, 250 };
-int sum = 0;
-int bin = 0;
-foreach(int items in inventory){
-  sum += items;
-  bin++;
-  Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+﻿/*
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs 
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
+*/
+Random random = new Random();
+string[] orderIDs = new string[5];
+
+for (int i = 0; i < orderIDs.Length; i++)
+{
+    int prefixValue = random.Next(65, 70);
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    string suffix = random.Next(1, 1000).ToString("000");
+
+    orderIDs[i] = prefix + suffix;
 }
-// Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
-Console.WriteLine($"We have {sum} items in inventory.");
+
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
+}
