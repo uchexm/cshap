3a0e386caa2f8b285732fef6eb5b8dcb70961d82
uchexm <109084069+uchexm@users.noreply.github.com>
2023-09-20 12:42:16 +0200

    update

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// Your code goes here
+if(daysUntilExpiration <= 10){
+  Console.WriteLine("Your subscription will expire soon. Renew now!");
+}else if (daysUntilExpiration <= 5){
+  Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+  discountPercentage = 10;
+}else if(daysUntilExpiration <= 1){
+  Console.WriteLine("Your subscription expires within a day!");
+discountPercentage = 20;
+}else if(daysUntilExpiration == 0){
+  Console.WriteLine("Your subscription has expired.");
+}
+else Console.WriteLine(discountPercentage);
